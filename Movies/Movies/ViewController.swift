f9bb479a23fe6e232d2cd21ec4755bd85a47f609
tavares <lucastavaresvasconcelos@gmail.com>
2019-12-09 15:37:34 -0300

    [ADD] Configure project to use ViewCode

---
+++ Movies/Movies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Lucas Tavares on 09/12/19.
+//  Copyright © 2019 Lucas Tavares. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = .yellow
+	}
+
+
+}
+
